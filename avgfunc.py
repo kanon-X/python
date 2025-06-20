7fceebcf68749e5307e13d7bb55167e605c79b50
kanon-X <syedur.b@uttarauniversity.edu.bd>
2025-06-20 23:43:44 +0600

    initial commit

---
+++ avgfunc.py
@@ -0,0 +1,8 @@
+def avg():
+    a =int(input("Enter your number: "))
+    b =int(input("Enter your number: "))
+    c =int(input("Enter your number: "))
+    average = (a+b+c)/3
+    print(average)
+
+avg()
