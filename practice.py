5c2c294f361c4c9516db939fcfcdaf35e848c724
kanon-X <syedur.b@uttarauniversity.edu.bd>
2025-10-10 04:15:27 +0600

    initial commit

---
+++ practice.py
@@ -0,0 +1,4 @@
+print("Thanks for using my program!")
+pname = input("what is the name of a pet? \n")
+bcity = input("what is the name of a city? \n")
+print(f"you are twitter handle and bio @cyber{pname} from {bcity}")
