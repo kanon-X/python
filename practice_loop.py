be27de4b9a1777630b459c1f895cd8ef9f60ad29
kanon-X <syedur.b@uttarauniversity.edu.bd>
2025-10-13 00:43:46 +0600

    initial commit

---
+++ practice_loop.py
@@ -0,0 +1,10 @@
+print("Fizzbuzz")
+for num in range(1,100):
+    if num%3==0 and num%5==0:
+        print("FizzBuzz")
+    elif num%5 ==0:
+        print("Buzz")
+    if num%3==0:
+        print("Fizz")
+    else:
+        print(num)
